6c6e1b8376e79e6f27e2990585aacbe72d148aa8
FTLE <91011574+ChiuAlfredo@users.noreply.github.com>
2021-11-26 09:55:20 +0800

    Add files via upload

--- MUSIC_FOLDER_RENAME.py
+++ MUSIC_FOLDER_RENAME.py
@@ -12,65 +12,91 @@ import os
 path ='D:\\music\\song_playlist'
 folder_name_list=os.listdir(path)
 
+with open('detail.txt','a',encoding = 'UTF-8') as f:
+    #分別進入各個資料夾
+    for folder_name in folder_name_list:
+        
+        #進入目錄的第一層
+        name = os.path.join(path,folder_name)
+        
+        #第二層目錄清單
+        folder_folder_list = os.listdir(name)
+        
+        #分別進入第二層資料夾
+        for folder_folder_name in folder_folder_list:
+            
+            #進入第三層資料夾
+            name1 = os.path.join(name,folder_folder_name)
+            
+            #偵測是否需要進入第三層
+            if os.path.isfile(name1)==0:
+                
+                #進入資料夾直到變成檔案
+                while os.path.isfile(name1)==0:
+                    
+                    #初始化name_fold給後面退出都是檔案的資料夾方便
+                    name_fold=name1
+                    
+                    #初始化list成第三層資料夾內容
+                    list = os.listdir(name1)
+                    
+                    #print資料夾內的音樂
+                    print(list)
+                    
+                    #如果第三層不是檔案，進入下一層
+                    if list != []:
+                        name1 = os.path.join(name1,list[0])
+                    else:
+                        break
+                    
+                    #print('888'+name1)
+                #顯示包含檔案的資料夾
+                print('最終資料夾 :'+name_fold)
+                
+            
+            #進入第二層就是包含音樂的資料夾
+            else:
+                
+                name_fold =os.path.abspath(os.path.dirname(name1))
+                print("不用進入"+name_fold)
+    
+            #正則表達式找出'MYFREEMP3.VIP'
+            #check_name = 'Porter Robinson - Mirror myfreemp3.vip '
+            print(os.path.join(name_fold,folder_folder_name))
+            #
+            #
+            # 
+            #
+            '''
+            regex = re.compile(r'(.*)+(myfreemp3.vip)')
 
-#分別進入各個資料夾
-for folder_name in folder_name_list:
-    
-    #進入目錄的第一層
-    name = os.path.join(path,folder_name)
-    
-    #第二層目錄清單
-    folder_folder_list = os.listdir(name)
-    
-    #分別進入第二層資料夾
-    for folder_folder_name in folder_folder_list:
+
+
         
-        #進入第三層資料夾
-        name1 = os.path.join(name,folder_folder_name)
+            check_name =folder_folder_name
+            print("check_name: ", check_name)
         
-        #偵測是否需要進入第三層
-        if os.path.isfile(name1)==0:
+            check = regex.search(check_name)
             
-            #進入資料夾直到變成檔案
-            while os.path.isfile(name1)==0:
+            #當regex配上時，寫入en_str,停止
+            if check != None:
+                print("進if")
+                print('regex : '+check.group(2))
+                          #os.rename(os.path.join(name_fold,folder_folder_name),os.path.join(name_fold,check.group(1)))
                 
-                #初始化name_fold給後面退出都是檔案的資料夾方便
-                name_fold=name1
-                
-                #初始化list成第三層資料夾內容
-                list = os.listdir(name1)
-                
-                #print資料夾內的音樂
-                print(list)
-                
-                #如果第三層不是檔案，進入下一層
-                if list != []:
-                    name1 = os.path.join(name1,list[0])
-                else:
-                    break
-                
-                #print('888'+name1)
-            #顯示包含檔案的資料夾
-            print('最終資料夾 :'+name_fold)
+            else:
+                print('REGEX:'+folder_folder_name)
+            '''
+
+
+            split_str = 'myfreemp3.vip'
+            check_name = folder_folder_name
+            if split_str in check_name:
+                new_name = check_name.split(split_str)
+                #f.write(''.join(new_name))
+                print("寫完ㄌ")
+                os.rename(os.path.join(name_fold,folder_folder_name),str(name_fold)+'\\'+''.join(new_name))
+            else:
+                print('不用')
+
             
-        
-        #進入第二層就是包含音樂的資料夾
-        else:
-            
-            name_fold =os.path.abspath(os.path.dirname(name1))
-            print("不用進入"+name_fold)
-        '''
-        #正則表達式找出'MYFREEMP3.VIP'
-        regex = re.compile(r'(.*)+(myfreemp3.vip)')
-        check_name = os.path.join(name_fold,folder_folder_name)
-        check = regex.search(check_name)
-        
-        
-        #當regex配上時，寫入en_str,停止
-        if check != None:
-            print(check.group(1))
-                      #os.rename(os.path.join(name_fold,folder_folder_name),os.path.join(name_fold,check.group(1)))
-            
-        else:
-            print(folder_folder_name)
-        '''
